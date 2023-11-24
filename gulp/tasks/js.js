3c29e1be890d981b873c1f9171395b0b8baec7af
ivan.malafeyev <ivan.malafeyev@gmail.com>
2023-11-24 12:28:55 +0400

    fix

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,6 +1,4 @@
 import webpack from "webpack-stream";
-//import fileInclude from "gulp-file-include";
-//import rename from "gulp-rename";
 
 export const js = () => {
 	return app.gulp
@@ -37,35 +35,3 @@ export const js = () => {
    .pipe(app.gulp.dest(app.path.build.js))
    .pipe(app.plugins.browsersync.stream());
 };
-
-/*export const js = () =>{
-	return app.gulp.src(`./src/js/app.min.js`, { sourcemaps: app.isDev })
-	.pipe(app.plugins.plumber(
-		app.plugins.notify.onError({
-			title: "JS",
-			message: "Error: <%= error.message %>"
-		})
-	))
-	.pipe(webpack({
-		mode: app.isBuild ? 'production' : 'development',
-		output: {
-			filename: 'app.min.js',
-		},
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					exclude: '/node_modules/',
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: [
-								'@babel/preset-env'
-							]
-						}
-					}
-		}]}
-}))
-	.pipe(app.gulp.dest(app.path.build.js))
-	.pipe(app.plugins.browsersync.stream());
-}*/
